258dabfaa2437e40acf98ce58522137021cf5734
Vanessa Cheang <cheang.c@northeastern.edu>
2023-06-11 17:54:00 -0700

    add radio button

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -28,7 +28,7 @@ fig.update_layout(
 fig.update_traces(line_color='#e29578')
 
 app.layout = html.Div(
-    style={'backgroundColor': '#edf6f9', 'fontFamily': 'Arial'},
+    style={'backgroundColor': '#edf6f9', 'fontFamily': 'Arial', 'margin-left': 'auto', 'padding': '10px'},
     children=[html.H1(
         style={'color': '#006d77'},
         children="Were sales higher before or after the Pink Morsel price increase on the 15th of January, 2021?"),
@@ -36,12 +36,46 @@ app.layout = html.Div(
         html.Div(style={'color': '#006d77'}, children='''
            The answer is obvious...YES!!
         '''),
-
+        dcc.RadioItems(
+            id='region-button',
+            options=[
+                {'label': 'North', 'value': 'north'},
+                {'label': 'East', 'value': 'east'},
+                {'label': 'South', 'value': 'south'},
+                {'label': 'West', 'value': 'west'},
+                {'label': 'All', 'value': 'all'}
+            ],
+            value='all',  # Default selected value
+            labelStyle={'display': 'inline-block', 'padding-top': '50px'}
+        ),
         dcc.Graph(
             id='pink-morsel-sale',
             figure=fig
         )
-    ])
+    ]
+)
+
+
+@app.callback(
+    Output(component_id='pink-morsel-sale', component_property='figure'),
+    Input(component_id='region-button', component_property='value')
+)
+def update_output_div(selected_region):
+    updated_fig = px.line(df, x="date", y="sales", title='Daily Sales of Pink Morsels')
+    if selected_region != 'all':
+        selected_row = df[df['region'] == selected_region]
+        updated_fig = px.line(selected_row, x="date", y="sales", title='Daily Sales of Pink Morsels')
+    updated_fig.update_layout(
+        title={'x': 0.5},
+        plot_bgcolor='#edf6f9',
+        paper_bgcolor='#edf6f9',
+        yaxis_range=[0, 3000],
+        font_color='#006d77',
+        font={'family': 'Arial'}
+    )
+    updated_fig.update_traces(line_color='#e29578')
+    return updated_fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
